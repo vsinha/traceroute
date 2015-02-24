02e6c14cac1db7ad101c069d0a14be0971d8f802
Viraj Sinha <virajosinha@gmail.com>
2015-02-24 11:18:09 -0500

    initial commit

---
+++ traceroute.py
@@ -0,0 +1,11 @@
+#!/usr/local/bin/python
+
+def main (dest):
+    while True:
+        # open connection
+        # print data
+        # break if useful
+        pass
+
+if __name__ == "__main__":
+    main("google.com")
